3a4de572b0efed072b327ea3eb94d346753442ba
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2024-02-21 15:09:58 -0500

    8 imu & 12 servo add

--- ROS_Arduino/include/config.h
+++ ROS_Arduino/include/config.h
@@ -12,17 +12,60 @@
 #include <Servo.h> 
 #include "Wire.h"
 
-#define DATA_IMU_LENGTH 2
-#define DATA_MULTIPLE_IMU_LENGTH 8
+#define DATA_IMU_LENGTH 8
+
+#define IMU_FEM_AV_D 0
+#define IMU_TIB_AV_D 1
+#define IMU_FEM_AV_G 2
+#define IMU_TIB_AV_G 3
+#define IMU_FEM_AR_D 4
+#define IMU_TIB_AR_D 5
+#define IMU_FEM_AR_G 6
+#define IMU_TIB_AR_G 7
+
+#define MOT_FEM_AV_D 2
+#define MOT_TIB_AV_D 3
+#define MOT_ABD_AV_D 4
+#define MOT_FEM_AV_G 5
+#define MOT_TIB_AV_G 6
+#define MOT_ABD_AV_G 7
+#define MOT_FEM_AR_D 8
+#define MOT_TIB_AR_D 9
+#define MOT_ABD_AR_D 10
+#define MOT_FEM_AR_G 11
+#define MOT_TIB_AR_G 12
+#define MOT_ABD_AR_G 13
+
+MPU6050 mpu_FEM_AV_D(Wire);
+MPU6050 mpu_TIB_AV_D(Wire);
+MPU6050 mpu_FEM_AV_G(Wire);
+MPU6050 mpu_TIB_AV_G(Wire);
+MPU6050 mpu_FEM_AR_D(Wire);
+MPU6050 mpu_TIB_AR_D(Wire);
+MPU6050 mpu_FEM_AR_G(Wire);
+MPU6050 mpu_TIB_AR_G(Wire);
+
+Servo FEM_AV_D;
+Servo TIB_AV_D;
+Servo ABD_AV_D;
+Servo FEM_AV_G;
+Servo TIB_AV_G;
+Servo ABD_AV_G;
+Servo FEM_AR_D;
+Servo TIB_AR_D;
+Servo ABD_AR_D;
+Servo FEM_AR_G;
+Servo TIB_AR_G;
+Servo ABD_AR_G;
+
+std_msgs::Float64MultiArray data_imu;
 
 void servo_cb(const std_msgs::UInt16 &cmd_msg);
 void TCA9548A(uint8_t bus);
 void readXYangles(std_msgs::Float64MultiArray* data_imu);
-
-std_msgs::Float64MultiArray data_imu;
-std_msgs::Float64MultiArray data_multi_imu;
-
-Servo Femur;
-Servo Tibia;
+float getXangles(uint8_t imu_num, MPU6050 imu_type);
+float getYangles(uint8_t imu_num, MPU6050 imu_type);
+void imu_init();
+void servo_init();
 
 #endif
