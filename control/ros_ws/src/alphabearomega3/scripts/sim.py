1ee78114c7604e11378b7dbcb775de14d330a99c
Revixa1 <txguimont@gmail.com>
2024-03-04 17:34:07 -0500

    updated sim feedback

--- control/ros_ws/src/alphabearomega3/scripts/sim.py
+++ control/ros_ws/src/alphabearomega3/scripts/sim.py
@@ -10,13 +10,21 @@ mutex=Lock()
 def twistCallback(BiData):    
         with mutex:
             for i in range(len(BiData.data)):
-                print(round(BiData.data[motor_IDs[i]]/360*(2*math.pi),4))
-                Motor_List[motor_IDs[i]].setPosition(round(BiData.data[motor_IDs[i]]/360*(2*math.pi),4))
+                #print(round(BiData.data[motor_IDs[i]]/360*(2*math.pi),4))
+                Motor_List[motor_IDs[i]].setPosition(round(math.radians(BiData.data[motor_IDs[i]]),4))
                 Motor_List[motor_IDs[i]].setVelocity(10)
                 #print()
         
-        
-        
+def enable_feedback():
+    for i in range(len(Position_List)):
+        Position_List[i].enable(10)
+     
+def get_feedback():
+
+    data=[0]*12
+    for i in range(len(Position_List)):
+        data[i]=Position_List[motor_IDs[i]].getValue()
+    return data
         
     
 
@@ -36,7 +44,21 @@ FL_shoulder_motor = robot.getDevice('FL_shoulder_servo')
 RR_shoulder_motor = robot.getDevice('RR_shoulder_servo')
 RL_shoulder_motor = robot.getDevice('RL_shoulder_servo')
 
+FR_femur_feedback = robot.getDevice('FR_femur_feedback')
+FL_femur_feedback = robot.getDevice('FL_femur_feedback')
+RR_femur_feedback = robot.getDevice('RR_femur_feedback')
+RL_femur_feedback = robot.getDevice('RL_femur_feedback')
+FR_tibia_feedback= robot.getDevice('FR_tibia_feedback')
+FL_tibia_feedback= robot.getDevice('FL_tibia_feedback')
+RR_tibia_feedback= robot.getDevice('RR_tibia_feedback')
+RL_tibia_feedback= robot.getDevice('RL_tibia_feedback')
+FR_shoulder_feedback = robot.getDevice('FR_shoulder_feedback')
+FL_shoulder_feedback = robot.getDevice('FL_shoulder_feedback')
+RR_shoulder_feedback = robot.getDevice('RR_shoulder_feedback')
+RL_shoulder_feedback = robot.getDevice('RL_shoulder_feedback')
+
 Motor_List=[FL_shoulder_motor,FL_femur_motor,FL_tibia_motor,FR_shoulder_motor,FR_femur_motor,FR_tibia_motor,RL_shoulder_motor,RL_femur_motor,RL_tibia_motor,RR_shoulder_motor,RR_femur_motor,RR_tibia_motor]
+Position_List=[FL_shoulder_feedback,FL_femur_feedback,FL_tibia_feedback,FR_shoulder_feedback,FR_femur_feedback,FR_tibia_feedback,RL_shoulder_feedback,RL_femur_feedback,RL_tibia_feedback,RR_shoulder_feedback,RR_femur_feedback,RR_tibia_feedback]
 motor_IDs=[]
 
 for i,motor in enumerate(Motor_List):
@@ -50,8 +72,13 @@ for i,motor in enumerate(Motor_List):
 rospy.init_node('sim', anonymous=True)
 sub= rospy.Subscriber('/Command',Float64MultiArray,twistCallback)
 pup= rospy.Publisher('/Feedback',Float64MultiArray,queue_size=10)
-        
+feedback_msg= Float64MultiArray()
+feedback_msg.data=[0]*12
+
+enable_feedback()        
 
 while robot.step(timeStep) != -1:
     with mutex:
+        feedback_msg.data=get_feedback()
+        pup.publish(feedback_msg)
         pass
