c5de6f18df9a1bc6f9528985217282a7d5206d8a
Marc-Olivier Fecteau <marc-olivier.fecteau@usherbrooke.ca>
2024-03-27 09:35:51 -0400

    adds functions for potentiometers feedback; not tested!

--- control/ROS_Arduino/src/pub_sub.cpp
+++ control/ROS_Arduino/src/pub_sub.cpp
@@ -1,15 +1,18 @@
 #include "config.h"
 #include <ros/time.h>
 #include <string.h>
-
-controls::BNO bno_array;
-
-ros::NodeHandle nh;
-ros::Publisher feedback("Feedback", &bno_array);
+#include <math.h>
 
 bool LED_ON = false;
 #define LED_PIN 22
 
+// ROS objects declarations
+ros::NodeHandle nh;
+ros::Publisher feedback("Feedback", &bno_array);
+ros::Publisher pot_feedback_pub("pot_feedback_topic", &pot_value_array);
+ros::Subscriber<controls::Servo_cmd> command("Command", servo_cmd);
+
+// TODO: ID and name this section
 uint64_t current_time = 0;
 int elapsed_time = 0;
 uint64_t prev_time = 0;
@@ -20,13 +23,12 @@ float ACCEL_VEL_TRANSITION = (float)(BNO055_SAMPLERATE_DELAY_MS) / 1000.0;
 float ACCEL_POS_TRANSITION = 0.5 * ACCEL_VEL_TRANSITION * ACCEL_VEL_TRANSITION;
 float xPos = 0, yPos = 0, heading_vel = 0, angleX = 0, angleY = 0, angleZ = 0;
 
-ros::Subscriber<controls::Servo_cmd> command("Command", servo_cmd);
-
 void setup()
 {
     nh.initNode();
     Serial.begin(115200);
     //servo_init();
+    pot_init(pot_id_array);
     BNO_init();
 
     nh.subscribe(command);
@@ -56,6 +58,12 @@ void servo_init(){
   ABD_AR_D.attach(MOT_ABD_AR_D);
   FEM_AR_D.attach(MOT_FEM_AR_D);
   TIB_AR_D.attach(MOT_TIB_AR_D);
+}
+
+void pot_init(const int* pot_id_array) {
+    for (uint8_t i = 0; i < NUMBER_OF_POTS; i++) {
+        pinMode(pot_id_array[i], INPUT);
+    }
 }
 
 void BNO_init() {
@@ -90,11 +98,8 @@ void servo_cmd(const controls::Servo_cmd &cmd_msg){
     
     delay(10);
     bno_update();
-    test_feedback(bno_array);
-}
-
-float get_angle(uint8_t pot_id) {
-    return analogRead(pot_id); // TODO: transform analog input into output range
+    bno_feedback(bno_array);
+    pot_feedback(pot_update(&pot_id_array));
 }
 
 void bno_update() {
@@ -119,7 +124,7 @@ void bno_update() {
     bno.getCalibration(&sys, &gyro, &accel, &mag);
 }
 
-void test_feedback(controls::BNO &feedback_array) {
+void bno_feedback(controls::BNO &feedback_array) {
     feedback_array.data[0] = xPos;
     feedback_array.data[1] = yPos;
     feedback_array.data[2] = heading_vel;
@@ -127,5 +132,20 @@ void test_feedback(controls::BNO &feedback_array) {
     feedback_array.data[4] = angleY;
     feedback_array.data[5] = angleZ;
     feedback.publish(&feedback_array);
+}
+
+controls::Servo_cmd pot_update(const int* pot_id_array) {
+    for (uint8_t i = 0; i < NUMBER_OF_POTS; i++) {
+        pot_value_array.data[i] = long2float_map(analogRead(pot_id_array[i]), POT_MIN_VALUE, POT_MAX_VALUE, POT_MIN_ANGLE, POT_MAX_ANGLE);
+    }
+    return pot_value_array;
+}
+
+void pot_feedback(controls::Servo_cmd pot_value_array) {
+    pot_feedback_pub.publish(&pot_value_array);
+}
+
+float long2float_map(long x, long IN_min, long IN_max, long OUT_min, long OUT_max) {
+    return ((float)x - (float)IN_min) * ((float)OUT_max - (float)OUT_min) / ((float)IN_max - (float)IN_min) + (float)OUT_min;
 }
 
